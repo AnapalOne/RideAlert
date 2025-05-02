6072412b95edbe3b95fc2af1fddbe430ab98d8c5
Anapal <91709385+AnapalOne@users.noreply.github.com>
2025-05-02 08:57:04 +0800

    Cleanup debug code
    Moved class declaration to `debug.h`. Might also move debug code to
    another file, if possible.
    
    @Andrei Jose R. Embarque <andreijosee@zoho.com>

--- PulseRider.ino
+++ PulseRider.ino
@@ -27,81 +27,81 @@
 MAX30105 MAX30105;
 ClosedCube_MAX30205 MAX30205;
 
-class 
-Debug {
-    public:
-      void
-      setupMAX30205 (uint8_t* address)
-      {
-        // Initialize the MAX30205 sensor
-        Serial.println("Starting MAX30205 module...");
-        MAX30205.begin(address);
-        Serial.println("Initialized MAX30205 module.");
-      }
-
-      void
-      setupMAX30105 ()
-      {
-        // Initialize the MAX30105 sensor
-        if (MAX30105.begin(Wire, I2C_SPEED_FAST) == false) {
-            Serial.println("MAX30105 failed to initialize. Please check wiring/power.");
-            while (1);
-        }
-        Serial.println("Started MAX30102. Setting up module...");
-        MAX30105.setup();
-        Serial.println("Initialized MAX30102 module.");
-      }
-
-      /*
-      * Get temperature from MAX30205, in Celsius.
-      * If readings consistently show 256.00C, sensor is probably malfunctioning.
-      */
-      float 
-      getTemperature_MAX30205()
-      {
-          return MAX30205.readTemperature();
-      }
-
-      /* Get temperature from MAX30102, in Celsius. */
-      void
-      setupBPM ()
-      {
-          MAX30105.setPulseAmplitudeRed(0x0A);
-          MAX30105.setPulseAmplitudeGreen(0);
-      }
-
-      /* Get BPM from MAX30102. */
-      float
-      getBPM ()
-      {
-          long irValue = MAX30105.getIR();
-
-          if (checkForBeat(irValue) == true)
-          {
-            //We sensed a beat!
-            long delta = millis() - lastBeat;
-            lastBeat = millis();
-        
-            beatsPerMinute = 60 / (delta / 1000.0);
-        
-            if (beatsPerMinute < 255 && beatsPerMinute > 20)
-            {
-              rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-              rateSpot %= RATE_SIZE; //Wrap variable
-        
-              //Take average of readings
-              beatAvg = 0;
-              for (byte x = 0 ; x < RATE_SIZE ; x++)
-                beatAvg += rates[x];
-              beatAvg /= RATE_SIZE;
-            }
-          }
-        
-          return (beatsPerMinute);
-      }
-};
-
 Debug debug;
+
+Debug::Debug ()
+{
+}
+
+void
+Debug::setupMAX30205 (uint8_t* address)
+{
+  // Initialize the MAX30205 sensor
+  Serial.println("Starting MAX30205 module...");
+  MAX30205.begin(address);
+  Serial.println("Initialized MAX30205 module.");
+}
+
+void
+Debug::setupMAX30105 ()
+{
+  // Initialize the MAX30105 sensor
+  if (MAX30105.begin(Wire, I2C_SPEED_FAST) == false) {
+      Serial.println("MAX30105 failed to initialize. Please check wiring/power.");
+      while (1);
+  }
+  Serial.println("Started MAX30102. Setting up module...");
+  MAX30105.setup();
+  Serial.println("Initialized MAX30102 module.");
+}
+
+/*
+* Get temperature from MAX30205, in Celsius.
+* If readings consistently show 256.00C, sensor is probably malfunctioning.
+*/
+float 
+Debug::getTemperature_MAX30205()
+{
+    return MAX30205.readTemperature();
+}
+
+/* Get temperature from MAX30102, in Celsius. */
+void
+Debug::setupBPM ()
+{
+    MAX30105.setPulseAmplitudeRed(0x0A);
+    MAX30105.setPulseAmplitudeGreen(0);
+}
+
+/* Get BPM from MAX30102. */
+float
+Debug::getBPM ()
+{
+    long irValue = MAX30105.getIR();
+
+    if (checkForBeat(irValue) == true)
+    {
+      //We sensed a beat!
+      long delta = millis() - lastBeat;
+      lastBeat = millis();
+  
+      beatsPerMinute = 60 / (delta / 1000.0);
+  
+      if (beatsPerMinute < 255 && beatsPerMinute > 20)
+      {
+        rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+        rateSpot %= RATE_SIZE; //Wrap variable
+  
+        //Take average of readings
+        beatAvg = 0;
+        for (byte x = 0 ; x < RATE_SIZE ; x++)
+          beatAvg += rates[x];
+        beatAvg /= RATE_SIZE;
+      }
+    }
+  
+    return (beatsPerMinute);
+}
 
 void 
 setup() 
--- src/debug.h
+++ src/debug.h
@@ -1,4 +1,19 @@
+#pragma once 
+
 #define ISDEBUG
+
+class
+Debug 
+{
+    public:
+        Debug ();
+
+        void setupMAX30205 (uint8_t* address);
+        void setupMAX30105 ();
+        float getTemperature_MAX30205();
+        void setupBPM ();
+        float getBPM ();
+};
 
 const byte RATE_SIZE = 5;
 byte rates[RATE_SIZE];
